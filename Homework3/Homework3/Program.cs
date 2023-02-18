1b9c1c2630b60db53b04ca3afa7a082dc9addf1f
rreich11 <96399337+rreich11@users.noreply.github.com>
2023-02-18 22:45:59 +0200

    first version of code, tested and works

---
+++ Homework3/Homework3/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("N \tN^2 \tN^3 \tN^2 + N^3");
+var endCondition = "";
+int n = 10;
+while (!endCondition.ToLower().Equals("x"))
+{
+
+    for (int x = 0; x < 5; x++)
+    {
+        int nn = n * n;
+        int nnn = nn * n;
+        Console.WriteLine(n + "\t" + nn + "\t" + nnn + " \t" + (nn + nnn));
+        n += 10;
+    }
+
+    Console.WriteLine("Press any key to continue, or x to end. ");
+    endCondition = Console.ReadLine();
+}
