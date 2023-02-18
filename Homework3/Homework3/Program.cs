d72a7e9a79fa3f6bebf0f6a26305c8bdf17c011b
rreich11 <96399337+rreich11@users.noreply.github.com>
2023-02-18 23:32:44 +0200

    end condition added to loop

--- Homework3/Homework3/Program.cs
+++ Homework3/Homework3/Program.cs
@@ -1,10 +1,9 @@
 ﻿
-// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("N \tN^2 \tN^3 \tN^2 + N^3");
-
+var endCondition = "";
 int n = 10;
-while (true)
+while (!endCondition.ToLower().Equals("x"))
 {
 
     for (int x = 0; x < 5; x++)
@@ -15,6 +14,7 @@ while (true)
         n += 10;
     }
 
-    Console.WriteLine("Press any key to continue");
-    Console.ReadLine();
+    Console.WriteLine("Press any key to continue, or x to end. ");
+    endCondition = Console.ReadLine();
 }
+
